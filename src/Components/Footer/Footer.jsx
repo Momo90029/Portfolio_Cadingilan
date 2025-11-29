7f4123b407c44d66d4809dce82a01eb6f76f3fa4
Omar bin Ayob D. Cadiniglan <omarcadingilan@gmail.com>
2025-11-29 21:11:24 +0800

    Responsive Update on home, and footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
       <footer className="bg-dark text-light border-top py-3 mt-auto">
       <div className="containerfooter d-flex justify-content-between align-items-center flex-wrap">
        
-      <div className="logo">
-          <h2 className="fs-5 fw-semibold mb-0 text-white">O.C</h2>
-        </div>
+     <div className="logo footer-logo">
+  <h2 className="fs-5 fw-semibold mb-0 text-white">O.C</h2>
+</div>
 
 
         {/* Left side (text & links) */}
@@ -22,18 +22,18 @@ const Footer = () => {
 
 
         {/* Right side (social icons) */}
-        <div className="d-flex align-items-center gap-3 me-4">
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-light">
-            <FaInstagram size={22} />
-          </a>
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-light">
+        <div className="social-links d-flex align-items-center gap-3 me-4">
+          <a href="https://www.facebook.com/Omarcadingilan01/" target="_blank" rel="noopener noreferrer" className="text-light">
             <FaFacebook size={22} />
           </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-light">
-            <FaLinkedin size={22} />
+          <a href="https://www.instagram.com/oms_void900" target="_blank" rel="noopener noreferrer" className="text-light">
+            <FaInstagram size={22} />
           </a>
-          <a href="https://github.com/Momo90029" target="-blank" rel="noopener noreferrer" className="text-light">
-            <FaGithub size={22}></FaGithub></a>
+          <a href="https://github.com/Momo90029" target="_blank" rel="noopener noreferrer" className="text-light">
+            <FaGithub size={22} />
+          </a>
+          <a href="https://www.linkedin.com/in/omar-bin-ayob-cadingilan-42b25b2ab/" target="-blank" rel="noopener noreferrer" className="text-light">
+            <FaLinkedin size={22}/></a>
         </div>
 
       </div>
