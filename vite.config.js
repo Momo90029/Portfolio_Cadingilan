096d5c390aeb5e831c0443bf87e9479d7d4de28b
Momo90029 <omarcadingilan@gmail.com>
2025-12-11 23:43:21 +0800

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
-import express from "express";
-import polishRoute from "./server/polish.js";
 
 // https://vite.dev/config/
 export default defineConfig({
